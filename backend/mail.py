ab741bebd3f48db5501cfdd8ff0bd332e0055802
Al hussein Khouma <alhusseinkhouma0@gmail.com>
2026-06-09 13:05:28 +0000

    Update mail.py

--- backend/mail.py
+++ backend/mail.py
@@ -1,23 +1,9 @@
-import smtplib
+compte gmail 
 
-# Configuration SMTP
-smtp_server = "smtp.gouv.sn"
-smtp_port = 25
-smtp_user = "gp-adm@adm.gouv.sn"
-smtp_password = "Gp@dm2026"
 
-try:
-    # Connexion au serveur SMTP
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()  # Active STARTTLS
-        server.ehlo()      # Salutation EHLO
-
-        # Authentification
-        server.login(smtp_user, smtp_password)
-        print("✅ Authentification réussie ! Les identifiants sont corrects.")
-except smtplib.SMTPAuthenticationError:
-    print("❌ Erreur : Authentification échouée. Vérifiez vos identifiants.")
-except smtplib.SMTPException as e:
-    print(f"❌ Erreur SMTP : {e}")
-except Exception as e:
-    print(f"❌ Erreur générale : {e}")
+SMTP_HOST=smtp.gmail.com
+SMTP_PORT=587
+SMTP_SECURE=false
+SMTP_USER=gp.adm.sn@gmail.com
+SMTP_PASS=aococpdbukbjgrfx
+SMTP_FROM="Gestion Planning <gp.adm.sn@gmail.com>"
